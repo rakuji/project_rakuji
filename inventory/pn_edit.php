6c0e1b6c447ba0d13b32e74066598659d67f55e2
rakuji <rakuji.github@gmail.com>
2022-05-02 23:51:28 +0800

    變更表格表單格式
    表格格式:pn_list.php
    表單格式:pn_edit.php

--- inventory/pn_edit.php
+++ inventory/pn_edit.php
@@ -20,8 +20,8 @@ include __DIR__ . '/../parts/html-head.php'; ?>
         <div class="col-12 col-md-10">
             <!-- <h2>首頁</h2> -->
             <?php
-            $systemName = '庫存管理系統';
-            $systemitem = '品號維護';
+            $systemName = '基本資料系統';
+            $systemitem = '產品資訊維護';
             include __DIR__ . '/../parts/breadcrumb.php'; ?>
 
             <!-- 分頁的內容:開始 -->
@@ -35,57 +35,63 @@ include __DIR__ . '/../parts/html-head.php'; ?>
             }
             ?>
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">修改品號資料</h5>
-                                <form name="form1" method="post" novalidate onsubmit="checkForm(); return false;">
-                                    <div class="mb-3">
-                                        <label for="pn" class="form-label">品號</label>
-                                        <input type="text" class="form-control" name="pn" value="<?= $row['product_id'] ?>" disabled>
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <input type="hidden" name="p_id" value="<?= $row['product_id'] ?>">
-                                    <div class="mb-3">
-                                        <label for="p_name" class="form-label"><b>* 名稱</b></label>
-                                        <input type="text" class="form-control" id="p_name" name="p_name" required value="<?= htmlentities($row['product_name']) ?>">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="p_desc" class="form-label">敘述</label>
-                                        <textarea class="form-control" id="p_desc" name="p_desc" rows="3"><?= htmlentities($row['product_desc']) ?></textarea>
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="p_price" class="form-label">單價</label>
-                                        <input type="tel" class="form-control" id="p_price" name="p_price" value="<?= $row['product_price'] ?>">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="p_cal" class="form-label">熱量</label>
-                                        <input type="text" class="form-control" id="p_cal" name="p_cal" value="<?= $row['product_cal'] ?>">
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="period" class="form-label">供應時段</label>
-                                        <select class="form-select" aria-label="Default select example" name="period" id="period">
-                                            <option value="1" <?= $row['period_id'] == 1 ? 'selected' : ''; ?>>不分時段</option>
-                                            <option value="2" <?= $row['period_id'] == 2 ? 'selected' : ''; ?>>午餐</option>
-                                            <option value="3" <?= $row['period_id'] == 3 ? 'selected' : ''; ?>>晚餐</option>
-                                            <option value="4" <?= $row['period_id'] == 4 ? 'selected' : ''; ?>>午晚餐</option>
-                                        </select>
-                                        <div class="form-text"></div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">修改</button>
-                                </form>
-                            </div>
-
+            <!-- <div class="container"> -->
+            <!-- <div class="row"> -->
+            <!-- <div class="col"> -->
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">修改品號資料</h5>
+                    <form class="row" name="form1" method="post" novalidate onsubmit="checkForm(); return false;">
+                        <!-- <div class="row"> -->
+                        <div class="mb-3 col-6">
+                            <label for="pn" class="form-label">品號</label>
+                            <input type="text" class="form-control" name="p_id" value="<?= $row['product_id'] ?>" readonly>
+                            <div class="form-text"></div>
                         </div>
-                    </div>
+                        <div class="mb-3 col-6">
+                            <label for="p_name" class="form-label"><b>* 名稱</b></label>
+                            <input type="text" class="form-control" id="p_name" name="p_name" required value="<?= htmlentities($row['product_name']) ?>">
+                            <div class="form-text"></div>
+                        </div>
+                        <!-- </div> -->
+                        <div class="mb-3">
+                            <label for="p_desc" class="form-label">敘述</label>
+                            <textarea class="form-control" id="p_desc" name="p_desc" rows="3"><?= htmlentities($row['product_desc']) ?></textarea>
+                            <div class="form-text"></div>
+                        </div>
+                        <!-- <div class="row"> -->
+                        <div class="mb-3 col-6">
+                            <label for="p_price" class="form-label">單價</label>
+                            <input type="tel" class="form-control" id="p_price" name="p_price" value="<?= $row['product_price'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="p_cal" class="form-label">熱量</label>
+                            <input type="text" class="form-control" id="p_cal" name="p_cal" value="<?= $row['product_cal'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+                        <!-- </div> -->
+                        <div class="mb-3 col-6">
+                            <label for="period" class="form-label">供應時段</label>
+                            <select class="form-select" aria-label="Default select example" name="period" id="period">
+                                <option value="1" <?= $row['period_id'] == 1 ? 'selected' : ''; ?>>不分時段</option>
+                                <option value="2" <?= $row['period_id'] == 2 ? 'selected' : ''; ?>>午餐</option>
+                                <option value="3" <?= $row['period_id'] == 3 ? 'selected' : ''; ?>>晚餐</option>
+                                <option value="4" <?= $row['period_id'] == 4 ? 'selected' : ''; ?>>午晚餐</option>
+                            </select>
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label for="p_image" class="form-label">照片</label>
+                            <input class="form-control" type="file" id="p_image" name="p_image">
+                        </div>
+                        <button class="btn btn-primary col-1" type="submit">修改</button>
+                    </form>
                 </div>
             </div>
+            <!-- </div> -->
+            <!-- </div> -->
+            <!-- </div> -->
             <!-- 分頁的內容:結束 -->
         </div>
     </div>
@@ -101,26 +107,26 @@ include __DIR__ . '/../parts/html-head.php'; ?>
     const p_cal_msg = p_cal.closest('.mb-3').querySelector('.form-text');
 
 
-    function checkForm(){
+    function checkForm() {
         let isPass = true; // 有沒有通過檢查
 
-        p_name_msg.innerText = '';  // 清空訊息
-        p_price_msg.innerText = '';  // 清空訊息
-        p_cal_msg.innerText = '';  // 清空訊息
+        p_name_msg.innerText = ''; // 清空訊息
+        p_price_msg.innerText = ''; // 清空訊息
+        p_cal_msg.innerText = ''; // 清空訊息
 
         // TODO: 表單資料送出之前, 要做格式檢查
 
-        if(p_name.value.length<2){
+        if (p_name.value.length < 2) {
             isPass = false;
             p_name_msg.innerText = '請填寫正確的品號名稱'
         }
-        
-        if(isNaN(p_price.value)){
+
+        if (isNaN(p_price.value)) {
             isPass = false;
             p_price_msg.innerText = '請填寫正確的單價整數值';
         }
-        
-        if(isNaN(p_cal.value)){
+
+        if (isNaN(p_cal.value)) {
             isPass = false;
             p_cal_msg.innerText = '請填寫正確的熱量整數值';
         }
@@ -133,22 +139,22 @@ include __DIR__ . '/../parts/html-head.php'; ?>
         //     }
         // }
 
-        if(isPass){
+        if (isPass) {
             const fd = new FormData(document.form1);
 
             fetch('pn_edit_api.php', {
-                method: 'POST',
-                body: fd
-            }).then(r => r.json())
-            .then(obj => {
-                console.log(obj);
-                if(obj.success){
-                    alert('修改成功');
-                    location.href = 'pn_list.php?pn=' + document.form1.pn.value;
-                } else {
-                    alert('修改失敗');
-                }
-            })
+                    method: 'POST',
+                    body: fd
+                }).then(r => r.json())
+                .then(obj => {
+                    console.log(obj);
+                    if (obj.success) {
+                        alert('修改成功');
+                        location.href = 'pn_list.php?pn=' + document.form1.pn.value;
+                    } else {
+                        alert('修改失敗');
+                    }
+                })
         }
     }
 </script>
--- inventory/pn_list.php
+++ inventory/pn_list.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/../parts/connect_db.php'; ?>
 
 <?php
-$title = '品號維護';
+$title = '產品資訊維護';
 include __DIR__ . '/../parts/html-head.php'; ?>
 
 <?php include __DIR__ . '/../parts/banner.php'; ?>
@@ -21,14 +21,14 @@ include __DIR__ . '/../parts/html-head.php'; ?>
         <div class="col-12 col-md-10">
             <!-- <h2>首頁</h2> -->
             <?php
-            $systemName = '庫存管理系統';
-            $systemitem = '品號維護';
+            $systemName = '基本資料系統';
+            $systemitem = '產品資訊維護';
             include __DIR__ . '/../parts/breadcrumb.php'; ?>
 
             <!-- 分頁的內容:開始 -->
 
             <?php
-            $perPage = 5; // 每一頁有幾筆
+            $perPage = 10; // 每一頁有幾筆
             $page = isset($_GET['page']) ? intval($_GET['page']) : 1;  // 用戶要看的頁碼
             if ($page < 1) {
                 header('Location: pn_list.php?page=1');
@@ -36,7 +36,7 @@ include __DIR__ . '/../parts/html-head.php'; ?>
             }
 
             $pn = isset($_GET['pn']) ? ($_GET['pn']) : '%';  // user在查詢文字方塊輸入的的品號
-            $t_sql = "SELECT COUNT(*) FROM product WHERE product_id like '{$pn}'";
+            $t_sql = "SELECT COUNT(*) FROM view_product WHERE product_id like '{$pn}'";
             // 取得總筆數
             $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
             $rows = []; // 預設沒有資料
@@ -52,111 +52,129 @@ include __DIR__ . '/../parts/html-head.php'; ?>
                 $sql = sprintf("SELECT * FROM view_product WHERE product_id LIKE '%s' ORDER BY product_id DESC LIMIT %s, %s", $pn, ($page - 1) * $perPage, $perPage);
                 $rows = $pdo->query($sql)->fetchAll(); // 拿到分頁資料
             }
-
             ?>
-
-            <div class="container ">
-                <div class="row">
-                    <div class="col">
-                        <div class="d-flex flex-row">
-                            <a href="pn_add.php">新增品號</a>
-                        </div>
-                        <div class="d-flex flex-row-reverse">
-                            <form class="d-flex mb-3" method="$_GET" action="pn_list.php">
-                                <input class="form-control me-2" name="pn" type="text" placeholder="品號(可搭配'%'字元 )" aria-label="Search" required>
-                                <button class="btn btn-primary search" type="submit">Search</button>
-                            </form>
-                        </div>
-                    </div>
+            <div class="d-flex justify-content-between">
+                <div>
+                    <a class="btn btn-primary" href="pn_add.php" role="button">新增品號</a>
                 </div>
-                <div class="row">
-                    <div class="col">
-                        <table class="table table-striped table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </th>
-                                    <th scope="col">品號</th>
-                                    <th scope="col">名稱</th>
-                                    <th scope="col">敘述</th>
-                                    <th scope="col">單價</th>
-                                    <th scope="col">供應時段</th>
-                                    <th scope="col">熱量-kcal</th>
-                                    <th scope="col">
-                                        <i class="fas fa-edit"></i>
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                if ($totalRows == 0) {
-                                    echo "<tr><td colspan='8'><b>查無品號!</b></td></tr>";
-                                } else { ?>
-                                    <?php foreach ($rows as $r) : ?>
-                                        <tr>
-                                            <td>
-                                                <a href="pn_delete.php?pn_del=<?= $r['product_id'] ?>" onclick="return confirm(`確定要刪除品號為 <?= $r['product_id'] ?> 的記錄嗎?`)">
-                                                    <!-- <a href="javascript: del_it(<?= $r['product_id'] ?>)"> -->
-                                                    <i class="fas fa-trash-alt"></i>
-                                                </a>
-                                            </td>
-                                            <td><?= $r['product_id'] ?></td>
-                                            <td><?= $r['product_name'] ?></td>
-                                            <td><?= $r['product_desc'] ?></td>
-                                            <td><?= $r['product_price'] ?></td>
-                                            <td><?= $r['period_name'] ?></td>
-                                            <td><?= $r['product_cal'] ?></td>
-                                            <td>
-                                                <a href="pn_edit.php?pn_edit=<?= $r['product_id'] ?>">
-                                                    <i class="fas fa-edit"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    <?php endforeach ?>
-                                <?php } ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col">
-                        <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
-                                    <a class="page-link" href="?page=<?= $page - 1 ?>&pn=<?= $pn ?>">
-                                        <i class="fas fa-arrow-alt-circle-left"></i>
-                                    </a>
-                                </li>
-                                <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
-                                    if ($i >= 1 and $i <= $totalPages) :
-                                ?>
-                                        <li class="page-item <?= $page == $i ? 'active' : '' ?>">
-                                            <a class="page-link" href="?page=<?= $i ?>&pn=<?= $pn ?>"><?= $i ?></a>
-                                        </li>
-                                <?php endif;
-                                endfor; ?>
-                                <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
-                                    <a class="page-link" href="?page=<?= $page + 1 ?>&pn=<?= $pn ?>">
-                                        <i class="fas fa-arrow-alt-circle-right"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-
+                <!-- <a href="pn_add.php">新增品號</a> -->
+                <form class="d-flex mb-3" method="$_GET" action="pn_list.php">
+                    <input class="form-control me-2" name="pn" type="text" placeholder="品號(可搭配'%'字元 )" aria-label="Search" required>
+                    <button class="btn btn-primary search" type="submit">Search</button>
+                </form>
             </div>
 
 
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            <i class="fas fa-trash-alt"></i>
+                        </th>
+                        <th scope="col">品號</th>
+                        <th scope="col">名稱</th>
+                        <th scope="col">敘述</th>
+                        <th scope="col">單價</th>
+                        <th scope="col">供應時段</th>
+                        <th scope="col">熱量-kcal</th>
+                        <th scope="col">照片</th>
+                        <th scope="col">
+                            <i class="fas fa-edit"></i>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if ($totalRows == 0) {
+                        echo "<tr><td colspan='8'><b>查無品號!</b></td></tr>";
+                    } else { ?>
+                        <?php foreach ($rows as $r) : ?>
+                            <tr>
+                                <td>
+                                    <a href="pn_delete.php?pn_del=<?= $r['product_id'] ?>" onclick="return confirm(`確定要刪除品號為 <?= $r['product_id'] ?> 的記錄嗎?`)">
+                                        <!-- <a href="javascript: del_it(<?= $r['product_id'] ?>)"> -->
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                </td>
+                                <td><?= $r['product_id'] ?></td>
+                                <td><?= $r['product_name'] ?></td>
+                                <?php
+                                if (strlen($r['product_desc']) >= 120) {
+                                    $product_desc = substr($r['product_desc'], 0, 120);
+                                    $product_desc = $product_desc . '<a href="#" data-bs-toggle="tooltip" title="' . substr($r['product_desc'], 120) . '">...</a>';
+                                } else {
+                                    $product_desc = str_pad($r['product_desc'], 120);
+                                }
+                                ?>
+                                <td><?= $product_desc ?></td>
+                                <td><?= $r['product_price'] ?></td>
+                                <td><?= $r['period_name'] ?></td>
+                                <td><?= $r['product_cal'] ?></td>
+                                <td>
+                                    <?php
+                                        $imgPath = '../imgs/products/' . substr($r['product_image'],0,2) . '/' . $r['product_image'];
+                                    ?>
+                                    <a href="#" data-bs-toggle="popover" data-bs-html="true" data-bs-content="<img src='<?= $imgPath ?>' width='200' />">
+                                        <?= $r['product_image'] ?>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="pn_edit.php?pn_edit=<?= $r['product_id'] ?>">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    <?php } ?>
+                </tbody>
+            </table>
+
+
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item <?= $page == 1 ? 'disabled' : '' ?>">
+                                <a class="page-link" href="?page=<?= $page - 1 ?>&pn=<?= $pn ?>">
+                                    <i class="fas fa-arrow-alt-circle-left"></i>
+                                </a>
+                            </li>
+                            <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
+                                if ($i >= 1 and $i <= $totalPages) :
+                            ?>
+                                    <li class="page-item <?= $page == $i ? 'active' : '' ?>">
+                                        <a class="page-link" href="?page=<?= $i ?>&pn=<?= $pn ?>"><?= $i ?></a>
+                                    </li>
+                            <?php endif;
+                            endfor; ?>
+                            <li class="page-item <?= $page == $totalPages ? 'disabled' : '' ?>">
+                                <a class="page-link" href="?page=<?= $page + 1 ?>&pn=<?= $pn ?>">
+                                    <i class="fas fa-arrow-alt-circle-right"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
         </div>
 
+
     </div>
+
 </div>
+</div>
+
 
 <!-- 分頁的內容:結束 -->
 
 <?php include __DIR__ . '/../parts/script.php'; ?>
 
 <?php include __DIR__ . '/../parts/html-foot.php'; ?>
+<script>
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+    $('["popover"]').popover();
+</script>
